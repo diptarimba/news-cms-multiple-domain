556dc39e4fc413a9c35de6e9fef4345d5c9ec4f5
Dipta <dipta.harimbawa@jatis.com>
2024-06-17 14:33:57 +0700

    [fix] domain seeder

--- database/seeders/URLMappingSeeder.php
+++ database/seeders/URLMappingSeeder.php
@@ -12,15 +12,139 @@ class URLMappingSeeder extends Seeder
      */
     public function run(): void
     {
-        $data = [
-            ['sub' => 'nasional', 'domain' => 'miracrate.my.id', 'name' => 'Miracrate Indonesi', 'code' => 'greymilk'],
-            ['sub' => 'yogyakarta', 'domain' => 'miracrate.my.id', 'name' => 'Miracrate Indodana', 'code' => 'oceanblue'],
-            ['sub' => 'bandung', 'domain' => 'miracrate.my.id', 'name' => 'Miracrate Indoraya', 'code' => 'antara'],
-            ['sub' => 'surabaya', 'domain' => 'miracrate.my.id', 'name' => 'Miracrate Indomilk', 'code' => 'redeye'],
+
+        $dataCreator = [
+            [
+                'domain' => 'navigasinusantara.com', 'name' => 'Navigasi Nusantara', 'code' => 'greymilk', 'sub' => [
+                    "nasional",
+                    "aceh",
+                    "balikpapan",
+                    "bandung",
+                    "belitung",
+                    "denpasar",
+                    "gorontalo",
+                    "indramayu",
+                    "jabar",
+                    "jatim",
+                    "kalimantan",
+                    "malang",
+                    "manado",
+                    "medan",
+                    "pekanbaru",
+                    "potianak",
+                    "jateng",
+                    "yogyakarta",
+                    "surabaya",
+                    "banten",
+                    "samarinda",
+                    "semarang"
+                ],
+            ], [
+                'domain' => 'listriknusantara.com', 'name' => 'Listrik Nusantara', 'code' => 'oceanblue', 'sub' => [
+                    "nasional",
+                    "balikpapan",
+                    "bandung",
+                    "belitung",
+                    "denpasar",
+                    "gorontalo",
+                    "indramayu",
+                    "jabar",
+                    "jatim",
+                    "jateng",
+                    "kalimantan",
+                    "malang",
+                    "manado",
+                    "medan",
+                    "pekanbaru",
+                    "potianak",
+                    "samarinda",
+                    "yogyakarta",
+                    "aceh",
+                    "surabaya"
+                ],
+            ], [
+                'domain' => 'listrikhijau.com', 'name' => 'Listrik Hijau', 'code' => 'antara', 'sub' => [
+                    "nasional",
+                    "aceh",
+                    "balikpapan",
+                    "bandung",
+                    "denpasar",
+                    "banten",
+                    "indramayu",
+                    "jabar",
+                    "jateng",
+                    "jatim",
+                    "kalimantan",
+                    "malang",
+                    "manado",
+                    "medan",
+                    "pekanbaru",
+                    "potianak",
+                    "samarinda",
+                    "yogyakarta",
+                    "surabaya",
+                    "gorontalo"
+                ],
+            ], [
+                'domain' => 'kompasenergi.com', 'name' => 'Kompas Energi', 'code' => 'redeye', 'sub' => [
+                    "nasional",
+                    "aceh",
+                    "balikpapan",
+                    "bandung",
+                    "banten",
+                    "belitung",
+                    "gorontalo",
+                    "indramayu",
+                    "denpasar",
+                    "jabar",
+                    "jateng",
+                    "jatim",
+                    "kalimantan",
+                    "malang",
+                    "medan",
+                    "manado",
+                    "pekanbaru",
+                    "potianak",
+                    "surabaya",
+                    "yogyakarta",
+                    "samarinda"
+                ],
+            ], [
+                'domain' => 'energiprimerindonesia.com', 'name' => 'Energi Primer Indonesia', 'code' => 'minima', 'sub' => [
+                    "nasional",
+                    "jabar",
+                    "kalimantan",
+                    "denpasar",
+                    "malang",
+                    "indramayu",
+                    "bandung",
+                    "aceh",
+                    "jateng",
+                    "medan",
+                    "potianak",
+                    "pekanbaru",
+                    "samarinda",
+                    "balikpapan",
+                    "manado",
+                    "jatim",
+                    "banten",
+                    "gorontalo",
+                    "surabaya",
+                    "yogyakarta"
+
+                ],
+            ],
         ];
 
-        foreach($data as $each){
-            \App\Models\URLMapping::firstOrCreate($each);
+        foreach ($dataCreator as $each) {
+            foreach($each['sub'] as $sub) {
+                \App\Models\URLMapping::firstOrCreate([
+                    'domain' => $each['domain'],
+                    'sub' => $sub,
+                    'name' => $each['name'],
+                    'code' => $each['code'],
+                ]);
+            }
         }
     }
 }
