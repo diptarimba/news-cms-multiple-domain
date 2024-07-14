ab9a55e03d2a11608b648c136a61fe7a2b25f076
Dipta Harimbawa <vipalkuda@gmail.com>
2024-07-14 14:35:05 +0700

    [fix] abort when not existing

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -38,11 +38,15 @@ class NewsController extends Controller
         unset($domain[0]);
         $domain = implode(".", $domain);
 
+        $news = Content::with('author')->where('slug', $slug)->first();
+        if (is_null($news)) {
+            abort(404);
+        }
+
         $template = URLMapping::where('sub', $subdomain)->where('domain', $domain)->first();
         $template = is_null($template) ? URLMapping::first(): $template;
         $templateCode = $template ? $template->code : 'greymilk';
         $category = Category::get()->pluck('name');
-        $news = Content::with('author')->where('slug', $slug)->first();
         $news->posted_at = Carbon::parse($news->posted_at)->format("d F Y");
         return view('page.news.show', compact('news', 'category', 'templateCode', 'template'));
     }
