ec8c0ecf44c8f4ad4831c87554d10e3df3b17d26
Dipta Harimbawa <dipta.harimbawa@bayarind.id>
2024-12-18 02:28:07 +0700

    try: with chunkz

--- app/Exports/DownloadContentByCode.php
+++ app/Exports/DownloadContentByCode.php
@@ -3,46 +3,39 @@
 namespace App\Exports;
 
 use App\Models\Content;
-use App\Models\URLMapping;
 use Carbon\Carbon;
-use Maatwebsite\Excel\Concerns\FromArray;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Maatwebsite\Excel\Concerns\FromQuery;
 use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
+use Maatwebsite\Excel\Concerns\WithChunkReading;
 
-class DownloadContentByCode implements FromArray, WithHeadings
+class DownloadContentByCode implements FromQuery, WithHeadings, WithMapping, ShouldQueue, WithChunkReading
 {
-    protected $url_map;
+    protected $code;
 
     public function __construct($code)
     {
-        $this->url_map = URLMapping::with(['contents' => function ($query) use ($code) {
-            $query->where('code', $code);
-            $query->select('title', 'posted_at', 'slug');
-        }])->whereHas('contents', function ($query) use ($code) {
-            $query->where('code', $code);
-        })->orderBy('domain')->get();
+        $this->code = $code;
     }
 
-    public function array(): array
+    public function query()
     {
-        $no = 1;
-        $combinedData = [];
-        foreach ($this->url_map as $eachURL) {
-            foreach ($eachURL->contents as $eachNews) {
-                if (trim($eachNews) == "") {
-                    continue;
-                }
-                $combinedData[] = [
-                    'No' => $no,
-                    'Date' => Carbon::parse($eachNews->posted_at)->format('d F Y'),
-                    'Title' => $eachNews->title,
-                    'URL' => $eachURL->sub . '.' . $eachURL->domain . '/show/'  . $eachNews->slug,
-                    'Media' => $eachURL->domain
-                ];
-                $no++;
-            }
-        }
+        return Content::query()->select('title', 'posted_at', 'slug')
+            ->where('code', $this->code)
+            ->with(['urlMapping'])
+            ->orderBy('posted_at');
+    }
 
-        return $combinedData;
+    public function map($content): array
+    {
+        return [
+            'No' => $content->id,
+            'Date' => Carbon::parse($content->posted_at)->format('d F Y'),
+            'Title' => $content->title,
+            'URL' => $content->urlMapping->sub . '.' . $content->urlMapping->domain . '/show/' . $content->slug,
+            'Media' => $content->urlMapping->domain,
+        ];
     }
 
     public function headings(): array
@@ -54,5 +47,10 @@ class DownloadContentByCode implements FromArray, WithHeadings
             'URL',
             'Media'
         ];
+    }
+
+    public function chunkSize(): int
+    {
+        return 1000; // Menyesuaikan ukuran chunk sesuai kebutuhan
     }
 }
